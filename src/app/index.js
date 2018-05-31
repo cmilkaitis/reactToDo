49fb5750178e0ddb5eb08a492d369918cba4a0b2
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-31 13:41:59 -0400

    first commit

---
+++ src/app/index.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+//Create component
+
+var TodoComponent = React.createClass({
+    render:function(){
+        return(
+            <h1>Helloooooooooo</h1>
+        );
+    }
+});
