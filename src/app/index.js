bdc13e5cc837b12aa88c5ee218d6af3efdb1c45d
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-06-01 22:11:47 -0400

    Lesson 5 - State and props

--- src/app/index.js
+++ src/app/index.js
@@ -4,17 +4,30 @@ var ReactDOM = require('react-dom');
 //Create component
 
 class TodoComponent extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            todos: ['first thing', 'second', 'another', 'one more']
+        }
+    }
+
     render(){
         return(
-            <div>
-                <h1>Helloooooooooo</h1>
-                <p>net ninja</p>
-                <p>{this.props.msg}</p>
+            <div id="todo-list">
+                <p>The busiest people have the most leisure...</p>
+                <ul>
+                    <li>{this.state.todos[0]}</li>
+                    <li>{this.state.todos[1]}</li>
+                    <li>{this.state.todos[2]}</li>
+                    <li>{this.state.todos[3]}</li>
+                </ul>
             </div>
         );
     }
 };
 
+
+
 //Add to html
 
-ReactDOM.render(<TodoComponent msg='The ninja likes cheese'/>, document.getElementById('todo-wrapper'));
+ReactDOM.render(<TodoComponent  />, document.getElementById('todo-wrapper'));
